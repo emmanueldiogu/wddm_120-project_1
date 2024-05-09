993874949d6ccdd33ed54c061ce89ca3c6def3d4
Emmanuel Diogu <emmanueldiogu@gmail.com>
2024-05-09 15:16:19 -0400

    my first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+/* 
+Content Replacement:
+TODO: When a user clicks on an item in the sidebar, change the content on the section in the main content area.
+*/
+/* 
+Style Alteration:
+TODO: Add a button that when clicked, changes the color of the page (e.g. background color, text color).
+*/
+/* 
+Element Creation and Deletion:
+TODO: Allow the user to add new items to the sidebar list and remove existing items.
+*/
+/* 
+Event Handling:
+TODO: Display a pop-up message when a user hovers the header.
+*/
