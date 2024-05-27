2c262fe172c9e1c176cc77c80ea009e49cf03fc5
Emmanuel Diogu <emmanueldiogu@gmail.com>
2024-05-27 15:07:20 -0400

    implement frontend and style alteration JavaScript

--- main.js
+++ main.js
@@ -2,14 +2,57 @@
 Content Replacement:
 TODO: When a user clicks on an item in the sidebar, change the content on the section in the main content area.
 */
+
 /* 
 Style Alteration:
-TODO: Add a button that when clicked, changes the color of the page (e.g. background color, text color).
+Add a button that when clicked, changes the color of the page (e.g. background color, text color).
 */
+const colorSchemeToggle = document.getElementById('checkbox');
+const toggleSwitch = document.querySelector('.ball');
+
+window.addEventListener('DOMContentLoaded', () => {
+    let colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    if (colorScheme === 'dark') {
+        colorSchemeToggle.checked = true;
+        toggleSwitch.style.transform = 'translate(100%)';
+    }
+    loadColorScheme(colorScheme);
+
+    // loadColorScheme(getCurrentColorScheme());
+})
+
+colorSchemeToggle.addEventListener('change', function () {
+    let colorScheme = getCurrentColorScheme();
+    if (this.checked) {
+        // If checked, apply dark mode
+        colorScheme = 'dark';
+        toggleSwitch.style.transform = 'translate(100%)';
+    } else {
+        // If not checked, remove dark mode
+        colorScheme = 'light';
+        toggleSwitch.style.transform = 'translate(0%)';
+    }
+    localStorage.setItem('emmanuel.colorScheme', `${colorScheme}`);
+    console.log(localStorage.getItem('emmanuel.colorScheme'));
+    loadColorScheme(colorScheme);
+});
+
+function getCurrentColorScheme() {
+    let colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    localStorage.getItem('emmanuel.colorScheme') ? colorScheme = localStorage.getItem('emmanuel.colorScheme') : null;
+    return colorScheme;
+}
+
+function loadColorScheme(colorScheme) {
+    const root = document.querySelector(':root');
+    root.setAttribute('color-scheme', `${colorScheme}`);
+}
+
 /* 
 Element Creation and Deletion:
 TODO: Allow the user to add new items to the sidebar list and remove existing items.
 */
+
 /* 
 Event Handling:
 TODO: Display a pop-up message when a user hovers the header.
