2bed618efafc6be60f3d8dc43c0a47c3b1323ea8
Emmanuel Diogu <emmanueldiogu@gmail.com>
2024-05-29 02:24:23 -0400

    done

--- main.js
+++ main.js
@@ -1,7 +1,47 @@
 /* 
+Listen for windows load 
+call the necessary functions
+ */
+window.addEventListener('DOMContentLoaded', () => {
+    popover();
+    let colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    if (colorScheme === 'dark') {
+        colorSchemeToggle.checked = true;
+        toggleSwitch.style.transform = 'translate(100%)';
+    }
+    loadColorScheme(colorScheme);
+});
+
+/* 
 Content Replacement:
-TODO: When a user clicks on an item in the sidebar, change the content on the section in the main content area.
+When a user clicks on an item in the sidebar, change the content on the section in the main content area.
 */
+const cityList = document.getElementById('side-list');
+const currentCity = document.getElementById('current-city');
+// Listen for click
+cityList.addEventListener('click', function (event) {
+    if (event.target.tagName === "LI") {
+        let element = getTextWithoutChildren(event.target);
+        currentCity.textContent = element;
+    }
+    if (event.target.tagName == "BUTTON") {
+        if (event.target.parentElement.parentElement.childElementCount > 1) {
+            event.target.parentElement.remove();
+            currentCity.textContent = "Current City"
+        }
+    }
+});
+
+// function to remove child elements
+function getTextWithoutChildren(element) {
+    let text = '';
+    for (let node of element.childNodes) {
+        if (node.nodeType === Node.TEXT_NODE) {
+            text += node.textContent;
+        }
+    }
+    return text.trim();
+}
 
 /* 
 Style Alteration:
@@ -10,30 +50,18 @@ Add a button that when clicked, changes the color of the page (e.g. background c
 const colorSchemeToggle = document.getElementById('checkbox');
 const toggleSwitch = document.querySelector('.ball');
 
-window.addEventListener('DOMContentLoaded', () => {
-    let colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    if (colorScheme === 'dark') {
-        colorSchemeToggle.checked = true;
-        toggleSwitch.style.transform = 'translate(100%)';
-    }
-    loadColorScheme(colorScheme);
-
-    // loadColorScheme(getCurrentColorScheme());
-})
-
 colorSchemeToggle.addEventListener('change', function () {
     let colorScheme = getCurrentColorScheme();
     if (this.checked) {
-        // If checked, apply dark mode
+        // If checked, apply dark and translate 100%
         colorScheme = 'dark';
         toggleSwitch.style.transform = 'translate(100%)';
     } else {
-        // If not checked, remove dark mode
+        // If not checked, apply light and translate 0%
         colorScheme = 'light';
         toggleSwitch.style.transform = 'translate(0%)';
     }
     localStorage.setItem('emmanuel.colorScheme', `${colorScheme}`);
-    console.log(localStorage.getItem('emmanuel.colorScheme'));
     loadColorScheme(colorScheme);
 });
 
@@ -50,10 +78,68 @@ function loadColorScheme(colorScheme) {
 
 /* 
 Element Creation and Deletion:
-TODO: Allow the user to add new items to the sidebar list and remove existing items.
+Allow the user to add new items to the sidebar list and remove existing items.
 */
+const cityInput = document.getElementById('city');
+const inputBtn = document.getElementById('add-city');
+
+// Listen for click of the add button
+inputBtn.addEventListener('click', () => {
+    if (cityInput.value.length > 1) {
+        addSidebarList(cityInput.value);
+        cityInput.value = '';
+    }
+})
+
+// Listen for keypress event of the enter key
+cityInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        if (cityInput.value.length > 1) {
+            addSidebarList(cityInput.value);
+            cityInput.value = '';
+        }
+    }
+});
+
+/* 
+    Function to append city name, 
+    it takes in the city name as a parameter
+*/
+const addSidebarList = function (city) {
+    const li = document.createElement('li');
+    var textNode = document.createTextNode(city); // Add your text here
+
+
+    // Create the button element
+    const button = document.createElement('button');
+    button.textContent = 'x'; // Set the button text
+    li.appendChild(textNode);
+    li.appendChild(button);
+    cityList.appendChild(li);
+};
+
+const resetInput = () => {
+
+}
 
 /* 
 Event Handling:
-TODO: Display a pop-up message when a user hovers the header.
+Display a pop-up message when a user hovers the header.
 */
+const popover = function () {
+    var popoverElements = document.querySelectorAll('.popover');
+
+    popoverElements.forEach(function (elem) {
+        elem.addEventListener('mouseenter', function () {
+            var content = this.getAttribute('data-popover-content');
+            var popoverDiv = document.createElement('div');
+            popoverDiv.classList.add('popover-content');
+            popoverDiv.textContent = content;
+            this.appendChild(popoverDiv);
+        });
+
+        elem.addEventListener('mouseleave', function () {
+            this.removeChild(this.querySelector('.popover-content'));
+        });
+    });
+};
